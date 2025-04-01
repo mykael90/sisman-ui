236472eea395426031c723d43b7dee45f74f2b71
Mykael Mello <eng.mykael@gmail.com>
2025-04-01 14:32:04 -0300

    implementação login SIPAC ok, falta apenas puxar mais informações, como foto

--- src/app/api/auth/_stiProvider.ts
+++ src/app/api/auth/_stiProvider.ts
@@ -1,4 +1,5 @@
 import { OAuthUserConfig, OAuthConfig } from 'next-auth/providers/oauth';
+import { env } from 'process';
 
 export interface STIProfile {
   sub: string;
@@ -54,26 +55,42 @@ export default function STIOAuthProvider<P extends STIProfile>(
       async request({ tokens }) {
         console.log(`tokens STI: ${JSON.stringify(tokens)}`);
 
-        if (!tokens.id_token) {
-          throw new Error('ID Token não encontrado!');
-        }
+        const userInfoResponse = await fetch(process.env.STI_USERINFO_URL, {
+          headers: {
+            Authorization: `Bearer ${tokens.access_token}`,
+            'X-API-Key': process.env.STI_XAPI_KEY
+          }
+        });
 
-        // Decodifica o ID Token manualmente para obter os dados do usuário
-        const decoded = JSON.parse(atob(tokens.id_token.split('.')[1]));
+        const userInfo = await userInfoResponse.json();
+
+        console.log(`userInfo STI: ${JSON.stringify(userInfo)}`);
 
         return {
-          sub: decoded.sub,
-          name: decoded.name || '',
-          email: decoded.email || ''
+          sub: userInfo['id-usuario'],
+          name: userInfo['nome-pessoa'] || '',
+          email: userInfo.email || ''
         };
       }
     },
-    profile(_, tokens) {
-      if (!tokens.id_token) {
-        throw new Error('ID Token não encontrado!');
-      }
+    profile(profile, tokens) {
+      console.log(`profile STI: ${JSON.stringify(profile)}`);
+      console.log(`tokens STI: ${JSON.stringify(tokens)}`);
+      let decoded = {};
 
-      const decoded = JSON.parse(atob(tokens.id_token.split('.')[1])); // Decodifica JWT
+      if (tokens.id_token) {
+        decoded = JSON.parse(
+          Buffer.from(tokens.id_token.split('.')[1], 'base64').toString()
+        );
+      } else {
+        console.log(`ID Token não encontrado: ${tokens.id_token}`);
+        // Use the sub from the userinfo response (profile)
+        decoded = {
+          sub: profile.sub,
+          name: profile.name || '',
+          email: profile.email || ''
+        };
+      }
 
       return {
         id: decoded.sub,
